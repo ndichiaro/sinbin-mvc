ce0d8b2c163a95e22fc0f815a46ffc3b611bd8f8
Nicholas DiChiaro <ndichiaro@gmail.com>
2017-02-05 06:58:03 -0500

    Add Red/Green Glow based off active status

--- Sinbin.Web/Controllers/FeedController.cs
+++ Sinbin.Web/Controllers/FeedController.cs
@@ -15,7 +15,7 @@ namespace Sinbin.Web.Controllers
             var tiles = new List<Tile>()
             {
                 new Tile { Image = "http://c.directlyrics.com/img/upload/selena-gomez-revival-promo-pic.jpg", Active = true },
-                new Tile { Image = "https://s-media-cache-ak0.pinimg.com/originals/a6/18/64/a6186459266bfaa86be41f49cb4ffd80.jpg", Active = true },
+                new Tile { Image = "https://s-media-cache-ak0.pinimg.com/originals/a6/18/64/a6186459266bfaa86be41f49cb4ffd80.jpg", Active = false },
                 new Tile { Image = "https://s-media-cache-ak0.pinimg.com/564x/30/2c/04/302c04caddb748657eb9edb65b76eb72.jpg", Active = true },
                 new Tile { Image = "https://s-media-cache-ak0.pinimg.com/originals/3c/7a/45/3c7a45141b07f8af548f262159b1edb3.jpg", Active = true }
             };

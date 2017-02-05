ee0240a7ad4b208814a1b85d49db9a6b3f122371
Nicholas DiChiaro <ndichiaro@gmail.com>
2017-02-05 06:09:33 -0500

    Add basic static feed
    Add feed that pulls static data from controller

---
+++ Sinbin.Web/Controllers/FeedController.cs
@@ -0,0 +1,26 @@
+﻿using Sinbin.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sinbin.Web.Controllers
+{
+    public class FeedController : Controller
+    {
+        // GET: Feed
+        public ActionResult Index()
+        {
+            var tiles = new List<Tile>()
+            {
+                new Tile { Image = "http://c.directlyrics.com/img/upload/selena-gomez-revival-promo-pic.jpg", Active = true },
+                new Tile { Image = "https://s-media-cache-ak0.pinimg.com/originals/a6/18/64/a6186459266bfaa86be41f49cb4ffd80.jpg", Active = true },
+                new Tile { Image = "https://s-media-cache-ak0.pinimg.com/564x/30/2c/04/302c04caddb748657eb9edb65b76eb72.jpg", Active = true },
+                new Tile { Image = "/https://s-media-cache-ak0.pinimg.com/originals/3c/7a/45/3c7a45141b07f8af548f262159b1edb3.jpg", Active = true }
+            };
+            
+            return View(tiles);
+        }
+    }
+}
---
+++ Sinbin.Web/Models/Tile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sinbin.Web.Models
+{
+    public class Tile
+    {
+        public string Image { get; set; }
+        public bool Active { get; set; }
+    }
+}

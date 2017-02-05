ab8e00c3134dd385fe16de5a3268f042e439eebd
Nicholas DiChiaro <ndichiaro@gmail.com>
2017-02-04 20:12:30 -0500

    Log In Styling
    Remove some default styles/tags from log in. Add style

--- Sinbin.Web/App_Start/BundleConfig.cs
+++ Sinbin.Web/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace Sinbin.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/login").Include(
+                      "~/Content/login.css"));
         }
     }
 }

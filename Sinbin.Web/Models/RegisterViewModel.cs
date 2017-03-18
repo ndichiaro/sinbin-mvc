3e8a98e7b6b1d65b3ad1389ce3ddbcb958db16c4
adu103 <adu103@gmail.com>
2017-03-18 17:41:26 -0400

    Small text changes

--- Sinbin.Web/Models/RegisterViewModel.cs
+++ Sinbin.Web/Models/RegisterViewModel.cs
@@ -32,7 +32,7 @@ namespace Sinbin.Web.Models
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Confirm Password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
